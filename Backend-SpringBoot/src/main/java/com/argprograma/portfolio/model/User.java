c0dc6c7a44825c6fd16e2ed7cdad113a4df591c3
Pablo Rule Calvi <prulec@gmail.com>
2022-05-02 01:24:02 -0300

    SpringBoot: creados paquetes básicos e inicializadas clases de model

---
+++ Backend-SpringBoot/src/main/java/com/argprograma/portfolio/model/User.java
@@ -0,0 +1,14 @@
+package com.argprograma.portfolio.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+}
---
+++ Backend-SpringBoot/src/main/java/com/argprograma/portfolio/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.argprograma.portfolio.repository;
+
+import com.argprograma.portfolio.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long> {
+    
+}
