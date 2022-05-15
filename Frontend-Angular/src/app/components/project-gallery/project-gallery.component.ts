597f30b5aae514d5455232fd94bca934078ac85b
Pablo Rule Calvi <prulec@gmail.com>
2022-05-15 05:36:56 -0300

    Angular: agregados botones de edición e inserción en Gallery, también marcador de selección de imagen

--- Frontend-Angular/src/app/components/project-gallery/project-gallery.component.ts
+++ Frontend-Angular/src/app/components/project-gallery/project-gallery.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { PORTFOLIO } from 'src/app/PORTFOLIO_CONST';
 import { Project } from 'src/app/Project';
+import { ProjectImage } from 'src/app/ProjectImage';
 
 @Component({
   selector: 'app-project-gallery',
@@ -10,6 +11,7 @@ import { Project } from 'src/app/Project';
 export class ProjectGalleryComponent implements OnInit {
 
   @Input() project:Project = PORTFOLIO.projectList[0];
+  @Input() selectedImage:ProjectImage = this.project.projectImageList[0];
   @Output() onCloseImagesWindow: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
@@ -19,6 +21,10 @@ export class ProjectGalleryComponent implements OnInit {
 
   onClose(){
     this.onCloseImagesWindow.emit();
+  }
+
+  onSelect(image:ProjectImage) {
+    this.selectedImage = image;
   }
 
 }
